ae8ff9e39884608674ff228d57d8d42652ce5a4c
allayor <htecgf@gmail.com>
2021-08-30 00:39:04 +0500

    Fixiing some bugs

--- console.js
+++ console.js
@@ -2,17 +2,20 @@ function Consol(container) {
 
     this.init(container)
 }
-CreateElement = (el) => document.createElement(el)
+
 Consol.prototype = (function() {
-
-            const css = {
-                color: 'darkred',
-                'font-weight': 'bold',
+            let CreateElement = (el, css) => {
+                let bufer = document.createElement(el)
+                bufer.setCss(css)
+                return bufer
             }
-
+            HTMLElement.prototype.setCss = function(css = {}) {
+                for (const key in css)
+                    this.style[key] = css[key]
+            }
             let theme = {
                 str: {
-                    color: '#d90f1f',
+                    color: '#d70011',
                 },
                 num: {
                     color: 'blue'
@@ -25,103 +28,84 @@ Consol.prototype = (function() {
             const makeEl = (value, type) => {
                     return `<span style='color:${theme[type].color}'>${type!=='str'?(`${value}`):(`"${value}"`)}</span>, `
     }
-
-    function makeHint(object) {
-            let isArr=Array.isArray(object)
-            let result = isArr?`Array(${object.length})[`:'{'
-            for (const key in object ){
-                result+=!isArr?(key+": "):''
-                switch(typeof object[key]){
-                    case 'string':
-                        result+=makeEl(object[key],'str')
-                        break
-                    case 'number':
-                        result+=makeEl(object[key],'num')
-                        break
-                    case 'object':
-                        if (Array.isArray(object[key])){
-                            if (object[key].length===0) result+='[], '
-                            else result+=`Array(${object[key].length}), `
-                        }else result+='{...}, '
-                        break
-                    case 'function':
-                        result+=makeEl('f','num')
-                        break 
-                    case 'boolean':
-                        result+=makeEl(object[key],'bool')
-                        break
-                }
-            }
-            return '<span >'+(result+(isArr?"]":'}')).replace(isArr?', ]':', }',isArr?']':'}')+'</span><br>'
+    function Collapse(e){
+        console.log(e)
     }
-    let arr=[collapse()]
-    function collapse(){
-        let m=CreateElement('div')
-        m.style.border='1px solid red'
-        m.style.padding='10px'
-        return m
-    }
-    function drawer(container, k, value, indent = 0,buf) {
-        // if (typeof value==='object'){
-        //     let collaps=collapse()
-        //     collaps.innerHTML=`<div>${k}: ${makeHint(value)}</div>`
-        //     let inner=collapse()
-        //     for (const m in value){
-        //         if (typeof value[m]!=='object')
-        //         inner.innerHTML+=`<div style='margin-left:${indent*5}px'>${m}: ${value[m]}</div>`
-        //         else
-        //         drawer(container,m,value[m],indent+4,buf)
-
-        //     }
-        //     collaps.appendChild(inner)
-        //     container.appendChild(collaps)
-            
-        // }
-        // if (typeof value === "object") {
-        //     container.innerHTML += `<span style='margin-left:${indent*6}px;color:#972485'> ${key}: </span>` + makeHint(value)
-        //     for (const key in value)
-        //         drawer(container, key, value[key], indent + 4)
-        // } else {
-        //     let val = typeof value === 'string' ? `<span style='color:#f34531'>"${value}"</span>` : value
-        //     container.innerHTML += `<span style='margin-left:${indent*6}px;color:#972485'> ` + key + `</span>` + ": " + val + '<br>'
-        // }
-  
-        for (const key in value){
-            if (typeof value[key]==='object'){
-                container.innerHTML+=`<div style='margin-left:${indent*5}px'>`+key+': '+makeHint(value[key])+'</div>'
-                drawer(container,key,value[key],indent+4)
-            }else{
-                let val
-                switch(typeof value[key]){
-                    case 'number':
-                        val=makeEl(value[key],'num')
-                        break
-                    case 'string':
-                        val=makeEl(value[key],'str')
-                        break
-                    case 'function':
-                        val=makeEl(value[key],'num')
-                        break
-                    case 'boolean':
-                        val=makeEl(value[key],'bool')
-                        break
-                }
-                container.innerHTML+=`<div style='margin-left:${indent*5}px'>`+key+': '+val.replace(',','')+'<br></div>'
+    function makeHint(object,bool=true) {
+        let isArr = Array.isArray(object)
+        let result = isArr ? `Array(${object.length})[` : '{'
+        for (const key in object) {
+            result += !isArr ? (key + ": ") : ''
+            switch (typeof object[key]) {
+                case 'string':
+                    result += makeEl(object[key], 'str')
+                    break
+                case 'number':
+                    result += makeEl(object[key], 'num')
+                    break
+                case 'object':
+                    if (Array.isArray(object[key])) {
+                        if (object[key].length === 0) result += '[], '
+                        else result += `Array(${object[key].length}), `
+                    } else result += '{...}, '
+                    break
+                case 'function':
+                    result += makeEl('f', 'num')
+                    break
+                case 'boolean':
+                    result += makeEl(object[key], 'bool')
+                    break
             }
-            // container.appendChild(arr[arr.length-1])
         }
-
-        
+        let dd=CreateElement('div')
+        let hint=CreateElement('div')
+        hint.className=bool?'collapse':'root'
+        hint.innerHTML=`<em>${(result + (isArr ? "]" : '}')).replace(isArr ? ', ]' : ', }', isArr ? ']' : '}')}</em>`
+        dd.appendChild(hint)
+     return dd.innerHTML
+    }
+    let keys=(key)=>{
+        return `<span style='color:#972485'>${key}</span>`
+    }
+    function grip() {
+        return `<svg width="14" height="14" viewBox="0 0 14 14" fill="none">
+        <path d="M9.78633 6.74709L4.58008 2.25724C4.38594 2.09044 4.10156 2.24083 4.10156 2.51017V11.4899C4.10156 11.7592 4.38594 11.9096 4.58008 11.7428L9.78633 7.25295C9.93535 7.12443 9.93535 6.8756 9.78633 6.74709Z" fill="#727272"/>
+        </svg>
+        `
+    }
+   
+    function drawer(value, indent = 0) {
+        let result = CreateElement('div',{height:0,overflow:'hidden'})
+        for (const key in value) {
+            if (typeof value[key] !== 'object') {
+                let val
+                switch (typeof value[key]) {
+                    case 'number':
+                        val = makeEl(value[key], 'num')
+                        break
+                    case 'string':
+                        val = makeEl(value[key], 'str')
+                        break
+                    case 'function':
+                        val = makeEl(value[key], 'num')
+                        break
+                    case 'boolean':
+                        val = makeEl(value[key], 'bool')
+                        break
+                }
+                let div=CreateElement('div',{marginLeft:`${indent*10}px`})
+                div.innerHTML=keys(key)+": "+val
+                result.appendChild(div)
+            } else {
+                let div=CreateElement('div',{marginLeft:`${indent*10}px`,display:'flex',alignItems:'center'})
+                div.innerHTML=grip()+keys(key)+": "+makeHint(value[key])
+                result.appendChild(div)
+                result.appendChild(drawer(value[key],indent+4))
+            }
+        }
+        return result
     }
 
-    // function span(data, color) {
-    //     let m = document.createElement('div')
-    //     let span = CreateElement('span')
-    //     span.style.color = color
-    //     span.innerText = data
-    //     m.appendChild(span)
-    //     return m.innerHTML
-    // }
     return {
         init(container) {
             this.consol = document.getElementById(container)
@@ -131,25 +115,32 @@ Consol.prototype = (function() {
             arg.forEach((item) => {
                 switch (typeof item) {
                     case "object":
-                        // this.consol.innerHTML += makeHint(item)
-                        // for (const i in item) {
-                        
-                        //     console.log(drawer(this.consol, i, item[i], 4))
-                        // }
-                   
-                        drawer(this.consol,0,item,0,arr)
-                       console.log(arr);
+                        let root = CreateElement('div')
+                        root.innerHTML=makeHint(item,false)
+                        root.addEventListener('click',function(){
+                            this.nextSibling.style.height='auto'
+                        })
+                        this.consol.appendChild(root)
+                        this.consol.appendChild(drawer(item, 0))
+                        let arr=document.getElementsByClassName('collapse')
+                        for (const a of arr){
+                            a.addEventListener('click',function(){
+                                this.getElementsByTagName('em')[0].style.visibility='hidden'
+                                this.parentElement.nextSibling.style.height='auto'
+                            })
+                        }
                         break
                     case 'string':
-                        this.consol.innerHTML+=makeEl(item,'str')+'&#9224;'
+                        this.consol.innerHTML += makeEl(item, 'str') + '&#9224;'
                         break
                     case 'number':
-                        this.consol.innerHTML+=makeEl(item,'num')
+                        this.consol.innerHTML += makeEl(item, 'num')
                         break
                     case 'function':
-                        this.consol.innerHTML+=makeEl(item,'num')
+                        this.consol.innerHTML += makeEl(item, 'num')
                 }
             })
+
         }
     }
 })()
