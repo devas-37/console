b9e60b1881806019509dbd54a48059950df5744f
allayor <htecgf@gmail.com>
2021-08-30 19:45:59 +0500

    Expand animation
    Expand animatsiya qo'shildi. Rekursiya optimizatisya qilindi

--- console.js
+++ console.js
@@ -15,28 +15,27 @@ Consol.prototype = (function() {
             }
             let theme = {
                 str: {
-                    color: '#d70011',
+                    color: '#3CEC85',
                 },
                 num: {
-                    color: 'blue'
+                    color: '#FF955C'
                 },
                 bool: {
-                    color: 'green'
+                    color: '#E35535'
                 }
             }
 
             const makeEl = (value, type) => {
                     return `<span style='color:${theme[type].color}'>${type!=='str'?(`${value}`):(`"${value}"`)}</span>, `
     }
-    function Collapse(e){
-        console.log(e)
-    }
-    function makeHint(object,bool=true) {
+
+    function makeHint(object, bool = true) {
         let isArr = Array.isArray(object)
         let result = isArr ? `Array(${object.length})[` : '{'
         for (const key in object) {
             result += !isArr ? (key + ": ") : ''
             switch (typeof object[key]) {
+
                 case 'string':
                     result += makeEl(object[key], 'str')
                     break
@@ -57,25 +56,31 @@ Consol.prototype = (function() {
                     break
             }
         }
-        let dd=CreateElement('div')
-        let hint=CreateElement('div')
-        hint.className=bool?'collapse':'root'
-        hint.innerHTML=`<em>${(result + (isArr ? "]" : '}')).replace(isArr ? ', ]' : ', }', isArr ? ']' : '}')}</em>`
+        let dd = CreateElement('div')
+        let hint = CreateElement('div')
+        hint.className = 'md'
+        hint.className = bool ? 'collapse' : 'root'
+        hint.innerHTML = `<span>${(result + (isArr ? "]" : '}')).replace(isArr ? ', ]' : ', }', isArr ? ']' : '}')}</span>`
         dd.appendChild(hint)
-     return dd.innerHTML
+        return dd.innerHTML
     }
-    let keys=(key)=>{
-        return `<span style='color:#972485'>${key}</span>`
+    let keys = (key) => {
+        return `<span style='color:white'>${key}</span>`
     }
+
     function grip() {
         return `<svg width="14" height="14" viewBox="0 0 14 14" fill="none">
         <path d="M9.78633 6.74709L4.58008 2.25724C4.38594 2.09044 4.10156 2.24083 4.10156 2.51017V11.4899C4.10156 11.7592 4.38594 11.9096 4.58008 11.7428L9.78633 7.25295C9.93535 7.12443 9.93535 6.8756 9.78633 6.74709Z" fill="#727272"/>
         </svg>
         `
     }
-   
+
     function drawer(value, indent = 0) {
-        let result = CreateElement('div',{height:0,overflow:'hidden'})
+        let result = CreateElement('div', {
+            overflow: 'hidden',
+            maxHeight: 0
+        })
+        result.className = 'expandable'
         for (const key in value) {
             if (typeof value[key] !== 'object') {
                 let val
@@ -93,14 +98,20 @@ Consol.prototype = (function() {
                         val = makeEl(value[key], 'bool')
                         break
                 }
-                let div=CreateElement('div',{marginLeft:`${indent*10}px`})
-                div.innerHTML=keys(key)+": "+val
+                let div = CreateElement('div', {
+                    marginLeft: `${indent*10}px`
+                })
+                div.innerHTML = keys(key) + ": " + val
                 result.appendChild(div)
             } else {
-                let div=CreateElement('div',{marginLeft:`${indent*10}px`,display:'flex',alignItems:'center'})
-                div.innerHTML=grip()+keys(key)+": "+makeHint(value[key])
+                let div = CreateElement('div', {
+                    marginLeft: `${indent*10}px`,
+                    display: 'flex',
+                    alignItems: 'center'
+                })
+                div.innerHTML = grip() + keys(key) + ": " + makeHint(value[key])
                 result.appendChild(div)
-                result.appendChild(drawer(value[key],indent+4))
+                result.appendChild(drawer(value[key], indent + 4))
             }
         }
         return result
@@ -112,32 +123,55 @@ Consol.prototype = (function() {
         },
         log() {
             let arg = Array.from(arguments)
+
             arg.forEach((item) => {
                 switch (typeof item) {
                     case "object":
                         let root = CreateElement('div')
-                        root.innerHTML=makeHint(item,false)
-                        root.addEventListener('click',function(){
-                            this.nextSibling.style.height='auto'
+                        root.innerHTML = makeHint(item, false)
+                        root.addEventListener('click', function() {
+                            this.nextSibling.style.maxHeight = this.nextSibling.scrollHeight + 100 + 'px'
+                            setTimeout(() => {
+                                this.nextSibling.style.maxHeight = 'fit-content'
+                            }, 500)
                         })
                         this.consol.appendChild(root)
                         this.consol.appendChild(drawer(item, 0))
-                        let arr=document.getElementsByClassName('collapse')
-                        for (const a of arr){
-                            a.addEventListener('click',function(){
-                                this.getElementsByTagName('em')[0].style.visibility='hidden'
-                                this.parentElement.nextSibling.style.height='auto'
+                        let arr = this.consol.getElementsByClassName('collapse')
+                        for (const a of arr) {
+                            a.addEventListener('click', function() {
+                                let el = this.parentElement.nextSibling
+                                if (el.getAttribute('state')==='open'){
+                                    el.style.maxHeight = 0
+                                    el.setAttribute('state','close')
+                                    this.style.opacity=1
+                                }else{
+                                    el.style.maxHeight = el.scrollHeight + 100 + 'px'
+                                    this.style.opacity=0
+                                    el.setAttribute('state','open')
+                                    setTimeout(function(){
+                                        el.style.maxHeight = 'fit-content'
+                                    }, 500)
+                                }
+                                
                             })
                         }
                         break
                     case 'string':
-                        this.consol.innerHTML += makeEl(item, 'str') + '&#9224;'
+                        let str = CreateElement('div')
+                        str.innerHTML = makeEl(item, 'str')
+                        this.consol.appendChild(str)
                         break
                     case 'number':
-                        this.consol.innerHTML += makeEl(item, 'num')
+                        let num = CreateElement('div')
+                        num.innerHTML = makeEl(item, 'num')
+                        this.consol.appendChild(num)
                         break
                     case 'function':
-                        this.consol.innerHTML += makeEl(item, 'num')
+                        let fn = CreateElement('div')
+                        fn.innerHTML = makeEl(item, 'num')
+                        this.consol.appendChild(fn)
+                        break
                 }
             })
 
--- main.js
+++ main.js
@@ -1,102 +1,60 @@
 let consol = new Consol('console')
 let data = [{
-    "title": "Bugungi ishlarmas",
-    "colors": {
-        "c1": "#D5F1E5",
-        "c2": "#1E704D"
-    },
-    "icon": "&#127929;",
-    "id": "G3490.2650747349029219",
-    "subTasks": [{
-        "title": "Aytilgan ishlar",
-        "complete": true,
-        "id": "S2780.991937861046434",
-        "steps": [{
-            "title": "Bazani tuzatish",
+        "title": "Bugungi ishlarmas",
+        "colors": {
+            "c1": "#D5F1E5",
+            "c2": "#1E704D"
+        },
+        "icon": "&#127929;",
+        "id": "G3490.2650747349029219",
+        "subTasks": [{
+            "title": "Aytilgan ishlar",
             "complete": true,
-            "id": "SSID2060.5592966308360912"
+            "id": "S2780.991937861046434",
+            "steps": [{
+                "title": "Bazani tuzatish",
+                "complete": true,
+                "id": "SSID2060.5592966308360912"
+            }, {
+                "title": "Test tayyorlash",
+                "complete": true,
+                "id": "SSID4780.7970626050623764"
+            }],
+            "sana": "2021- yil 27- avgust",
+            "vaqt": "15 : 19"
         }, {
-            "title": "Test tayyorlash",
+            "title": "tushdan keyingi ishlar",
             "complete": true,
-            "id": "SSID4780.7970626050623764"
-        }],
-        "sana": "2021- yil 27- avgust",
-        "vaqt": "15 : 19"
-    }, {
-        "title": "tushdan keyingi ishlar",
-        "complete": true,
-        "id": "S9260.6222230030261571",
-        "steps": [{
-            "title": "Nima qwilish",
-            "complete": true,
-            "id": "SSID2700.5684820128634183"
-        }],
-        "sana": "2021- yil 27- avgust",
-        "vaqt": "15 : 19"
-    }]
-}, {
-    "title": "Taqiqlangan ishlar",
-    "colors": {
-        "c1": "#FFE4E9",
-        "c2": "#AC395D"
-    },
-    "icon": "&#9940;",
-    "id": "G8980.8086892076613521",
-    "subTasks": [{
-        "title": "Qilmashlik",
-        "complete": true,
-        "id": "S6710.7373518805496864",
-        "steps": [],
-        "sana": "2021- yil 27- avgust",
-        "vaqt": "15 : 20"
-    }]
-}]
-consol.log(
-        [{
-            name: 'Mashina',
-            color: 'white',
-            number: {
-                region: 25,
-                letter: 'S',
-                number: 957,
-                tletter: [14, 151, 16]
-            }
-        }, {
-            name: 'Byscle',
-            color: 'black',
-            number: {
-                region: 1,
-                letter: 'X',
-                number: 700,
-                tletter: {
-                    item: '18',
-                    item2: '24'
-                }
-            }
+            "id": "S9260.6222230030261571",
+            "steps": [{
+                "title": "Nima qwilish",
+                "complete": true,
+                "id": "SSID2700.5684820128634183"
+            }],
+            "sana": "2021- yil 27- avgust",
+            "vaqt": "15 : 19"
         }]
-    )
+    }, {
+        "title": "Taqiqlangan ishlar",
+        "colors": {
+            "c1": "#FFE4E9",
+            "c2": "#AC395D"
+        },
+        "icon": "&#9940;",
+        "id": "G8980.8086892076613521",
+        "subTasks": [{
+            "title": "Qilmashlik",
+            "complete": true,
+            "id": "S6710.7373518805496864",
+            "steps": [],
+            "sana": "2021- yil 27- avgust",
+            "vaqt": "15 : 20"
+        }]
+    }]
+    // consol.log(
+    //         data
+    //     )
     // consol.log(console)
-    // console.log([{
-    //     name: 'Mashina',
-    //     color: 'white',
-    //     number: {
-    //         region: 25,
-    //         letter: 'S',
-    //         number: 957,
-    //         tletter: [14, 151, 16]
-    //     }
-    // }, {
-    //     name: 'Byscle',
-    //     color: 'black',
-    //     number: {
-    //         region: 1,
-    //         letter: 'X',
-    //         number: 700,
-    //         tletter: {
-    //             item: '18',
-    //             item2: '24'
-    //         }
-    //     }
-    // }])
+consol.log(data)
 
 console.log(data)
